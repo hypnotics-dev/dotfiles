7ab99eb097219593ced9f298524b38fbd1389222
hypnotics <devhypnotics@proton.me>
2024-09-07 13:41:45 -0300

    [emacs] Removed dirvish
    Removed the dirvish package.
    
    Updated some tasks

--- emacs/init.el
+++ emacs/init.el
@@ -185,7 +185,7 @@
          "\n* Synopsis\n\n%?\n* The Theory of %^{Name}\n\n* %^{Other|Implementation in Languages|In Emacs}\n\n* References"
          :if-new (file+head "${slug}-%<%Y%m%d%H%M%S>.org" "#+title: ${title}\n")
          :unnarrowed t)
-        ("c" "UNI Course" plain ;; Not working
+        ("c" "UNI Course" plain 
          "\nCourse Name: %^{name}\nCourse Id: %^{id}\nSection: %^{section}\nProfessor: %^{prof}\nLecture Classroom: %^{class}\nTutorial Classroom: %^{tutorial}\nLecture Times: %^{lecturetime}\nTutorial Time: %^{time}\nCredits: $^{cred}\nTerm Taken: $^{termtime}\n\n* Index of Topics\n\n\n* Homework\n\n%?\n\n* References\n"
          :if-new (file+head "${slug}-%<%Y%m%d%H%M%S>.org" "#+title: ${title}\n")
          :unnarrowed t)
@@ -203,44 +203,6 @@
 
 (use-package git-modes
   :after magit)
-
-(use-package dirvish
-  :init
-  (dirvish-override-dired-mode)
-  :custom
-  (dirvish-quick-access-entries
-   '(("h" "~"                 "Home")
-     ("d" "~/downloads"       "Downloads")
-     ))
-  :config
-  (setq dirvish-attributes
-        '(vc-state file-size file-time all-the-icons))
-  (setq dirvish-use-header-line 'global)
-  (setq dirvish-preview-dispatchers
-        (cl-substitute 'pdf-preface 'pdf dirvish-preview-dispatchers))
-  (setq dired-listing-switches
-        "-l --almost-all --human-readable --group-directories-first --no-group")
-  :bind
-  (("C-c f" . dirvish-fd)
-   :map dirvish-mode-map ; Dirvish inherits `dired-mode-map'
-   ("a"   . dirvish-quick-access)
-   ("f"   . dirvish-file-info-menu)
-   ("y"   . dirvish-yank-menu)
-   ("N"   . dirvish-narrow)
-   ("^"   . dirvish-history-last)
-   ("h"   . dirvish-history-jump) ; remapped `describe-mode'
-   ("s"   . dirvish-quicksort)    ; remapped `dired-sort-toggle-or-edit'
-   ("v"   . dirvish-vc-menu)      ; remapped `dired-view-file'
-   ("TAB" . dirvish-subtree-toggle)
-   ("M-f" . dirvish-history-go-forward)
-   ("M-b" . dirvish-history-go-backward)
-   ("M-l" . dirvish-ls-switches-menu)
-   ("M-m" . dirvish-mark-menu)
-   ("M-t" . dirvish-layout-toggle)
-   ("M-s" . dirvish-setup-menu)
-   ("M-e" . dirvish-emerge-menu)
-   ("M-j" . dirvish-fd-jump))
-  )
 
 (use-package vertico
   :custom
