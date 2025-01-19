b683460e6f4f0552978236e2240e4402e4a18a18
hypnotics <devhypnotics@proton.me>
2025-01-19 18:58:38 -0400

    [nvim] fixed missing elements from HEAD~1

--- editor/nvim/plugin/term/git.lua
+++ editor/nvim/plugin/term/git.lua
@@ -1,14 +1,15 @@
 local helpers = require("helpers.misc")
+local term = require("helpers.term")
 local map = helpers.map
 
 
 -- TODO: does not try and reuese the existing term 
 map("n", "<leader>gc", function ()
   -- TODO: Prevent execution if not in git buffer
-  if (vim.t.term_job == nil)then
-    helpers.make_small_term()
+  if (term.data.job_id == 0 or term.data.bufnr == 0)then
+    term.make_small_term()
   end
-  vim.fn.chansend(vim.t.term_job, "emacsclient -cte '(magit)'\r\n")
+  vim.fn.chansend(term.data.job_id, "emacsclient -cte '(magit)'\r\n")
 
 end, "[G]it [C]ommit")
 
