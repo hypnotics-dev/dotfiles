d6062d85d117978c13d35b53c61c087062ffd376
hypnotics <devhypnotics@proton.me>
2025-01-20 10:34:38 -0400

    [nvim] remove lazy file

--- editor/nvim/init.lua
+++ editor/nvim/init.lua
@@ -1,7 +1,18 @@
 require('config.settings')
-require('config.lazy')
-require('config.lsp')
-require('config.keymap')
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    { import = "plug" },
+  },
+  checker = { enabled = true },
+})
 
 -- Plugs
 -- https://github.com/stevearc/quicker.nvim
