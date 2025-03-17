7605b4337f80cb984612ad81b90e65d77af323b5
hypnotics <devhypnotics@proton.me>
2025-03-16 22:58:57 -0300

    [nvim] more ocaml stuff

--- editor/nvim/after/ftplugin/ocaml.lua
+++ editor/nvim/after/ftplugin/ocaml.lua
@@ -2,3 +2,12 @@
 -- TODO: Create code action to make a symbol public through its module file
 -- TODO: Keybind for dune build (should notify me with vim.notify of the result)
 
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+local map = require("helpers.misc").map
+local comp = require("helpers.compiler")
+
+map("n", "<leader><leader><return>", comp.dune_cont_builder,"Start Dune Builder")
